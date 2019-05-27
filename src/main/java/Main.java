15806f5b07b1c914cb8f5e5b0657bb8bff72bc1d
Tempert <github@tempert.nu>
2019-05-27 12:09:20 +0200

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,43 @@
-public class Main {
+import model.Address;
+import model.Company;
 
-    public static void main (String[] args) {
+public class Main
+{
+
+    public static void main (String[] args)
+    {
+        var main = new Main();
+        var model = main.GetCompany();
+
+        //parse
+
         System.out.println("Hello world!");
     }
+
+    private Main()
+    {
+        //ignore
+    }
+
+    public Company GetCompany()
+    {
+        var companyAddress = new Address();
+        companyAddress.setCity("Utrecht");
+        companyAddress.setPostalCode("1234aa");
+        companyAddress.setStreet("Street");
+        companyAddress.setStreetNumber(21);
+
+        var company = new Company();
+        company.setAddress(companyAddress);
+        company.setName("Company name");
+        company.setVatNumber("02349385");
+        company.setIban("INGB03NL0001234435");
+        company.setBic("ING");
+
+        return company;
+    }
+
+    private void
+
 
 }
--- src/main/java/model/Address.java
+++ src/main/java/model/Address.java
@@ -7,4 +7,43 @@ public class Address
 	private String postalCode;
 	private String city;
 
+	public String getStreet()
+	{
+		return street;
+	}
+
+	public void setStreet(String street)
+	{
+		this.street = street;
+	}
+
+	public int getStreetNumber()
+	{
+		return streetNumber;
+	}
+
+	public void setStreetNumber(int streetNumber)
+	{
+		this.streetNumber = streetNumber;
+	}
+
+	public String getPostalCode()
+	{
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode)
+	{
+		this.postalCode = postalCode;
+	}
+
+	public String getCity()
+	{
+		return city;
+	}
+
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
 }
