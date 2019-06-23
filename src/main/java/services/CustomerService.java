db8a9d868bbc4fe9a0308cbb7011ac6ac19ccc88
Nick W <nickwindt@hotmail.nl>
2019-06-23 22:02:07 +0200

    Implemented EasyMock Unit Test for CustomerService.

--- src/main/java/services/CustomerService.java
+++ src/main/java/services/CustomerService.java
@@ -2,6 +2,7 @@ package services;
 
 import com.github.dozermapper.core.Mapper;
 import model.Customer;
+import org.hibernate.SessionFactory;
 import persistence.mysql.MySqlUtil;
 import persistence.mysql.dao.CustomerDao;
 import services.generic.ICustomerService;
@@ -11,8 +12,13 @@ import java.util.List;
 
 public class CustomerService implements ICustomerService {
 
+    private SessionFactory sessionFactory;
+    public CustomerService(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     public List<Customer> getAll() {
-        var session = MySqlUtil.getSessionFactory().openSession();
+        var session = this.sessionFactory.openSession();
         var customerDaos = session.createQuery("from CustomerDao ", CustomerDao.class).list();
         var customers = new ArrayList<Customer>();
         for (var customerDao : customerDaos) {
