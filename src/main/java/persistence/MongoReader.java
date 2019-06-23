f8c0cbbce1d9333f0e3dacea6ff3ba9b4338fcad
Nynke van Koningsveld <NynkevK@users.noreply.github.com>
2019-06-23 17:20:22 +0200

    Little bit of refactoring and trying to make it work

--- src/main/java/persistence/MongoReader.java
+++ src/main/java/persistence/MongoReader.java
@@ -7,7 +7,6 @@ import model.InvoiceLine;
 import org.bson.Document;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,39 +14,55 @@ public class MongoReader {
     private ArrayList<Invoice> invoices = new ArrayList<Invoice>();
 
     public ArrayList<Invoice> getAllInvoices () {
+        MongoCollection<Document> mongoCollection = connectToDatabase();
+
+        FindIterable<Document> documents = mongoCollection.find();
+
+        for (Document document : documents){
+            fillInvoice(document);
+        }
+
+        return invoices;
+    }
+
+    public MongoCollection<Document> connectToDatabase() {
         MongoClientURI uri = new MongoClientURI("mongodb+srv://Nynke:Tester123@clusterfriendspammer-fvgbf.mongodb.net/test?retryWrites=true&w=majority");
 
-        try (MongoClient mongoClient = new MongoClient(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("BiFiBEP02");
+        MongoClient mongoClient = new MongoClient(uri);
 
-            MongoCollection<Document> mongoCollection = database.getCollection("bifi");
+        MongoDatabase database = mongoClient.getDatabase("BiFiBEP02");
 
-            FindIterable<Document> documents = mongoCollection.find();
+        MongoCollection<Document> mongoCollection = database.getCollection("bifi");
 
-            for (Document document : documents){
-                Invoice invoice = new Invoice();
-                invoice.setCustomerId(document.getInteger("customerId"));
-                invoice.setDate(document.getDate("date"));
-                invoice.setInvoiceId(document.getInteger("invoiceId"));
-                invoice.setNote(document.getString("note"));
-                invoice.setPersonId(document.getInteger("personId"));
-                List<Document> invoiceLinesfromDb = (List<Document>) document.get("invoiceLines");
-                for (Document line : invoiceLinesfromDb) {
-                    InvoiceLine invoiceLine = new InvoiceLine();
-                    invoiceLine.setBtwCode(line.getString("btwCode"));
-                    invoiceLine.setProductId(line.getInteger("productId"));
-                    invoiceLine.setProductName(line.getString("productName"));
-                    invoiceLine.setQuantity(line.getInteger("quantity"));
-                    invoiceLine.setTotalPrice(line.getInteger("totalPrice"));
-                    invoiceLine.setUnit(line.getString("unit"));
-                    invoice.setInvoiceLine(invoiceLine);
-                }
-                invoices.add(invoice);
-            }
+        return mongoCollection;
+    }
+
+    //@Luuk zie in de test dat ik de connectie wil testen, maar weet niet helemaal hoe en wat en of ik moet mocken oid, omdat ik een MongoCollection terug geef, maar weet niet goed hoe dat er dan eigenlijk uit ziet.
+
+    public void fillInvoice(Document document) {
+        Invoice invoice = new Invoice();
+        invoice.setCustomerId(document.getInteger("customerId"));
+        invoice.setDate(document.getDate("date"));
+        invoice.setInvoiceId(document.getInteger("invoiceId"));
+        invoice.setNote(document.getString("note"));
+        invoice.setPersonId(document.getInteger("personId"));
+        List<Document> invoiceLinesfromDb = (List<Document>) document.get("invoiceLines");
+        for (Document line : invoiceLinesfromDb) {
+            fillInvoiceLines(line, invoice);
         }
-        catch (MongoException mongoException) {
-            mongoException.printStackTrace();
-        }
-        return invoices;
+        invoices.add(invoice);
+    }
+
+    //@Luuk zie ook hiervoor bij de tests, moet ik mocken? Of gewoon iets instantiëren? Twijfel hierover. Zelfde geldt voor de methode hierboven en hieronder.
+
+    public void fillInvoiceLines(Document line, Invoice invoice) {
+        InvoiceLine invoiceLine = new InvoiceLine();
+        invoiceLine.setBtwCode(line.getString("btwCode"));
+        invoiceLine.setProductId(line.getInteger("productId"));
+        invoiceLine.setProductName(line.getString("productName"));
+        invoiceLine.setQuantity(line.getInteger("quantity"));
+        invoiceLine.setTotalPrice(line.getInteger("totalPrice"));
+        invoiceLine.setUnit(line.getString("unit"));
+        invoice.setInvoiceLine(invoiceLine);
     }
 }
--- src/test/java/persistence/MongoReaderTester.java
+++ src/test/java/persistence/MongoReaderTester.java
@@ -1,12 +1,19 @@
 package persistence;
 
+import com.mongodb.client.MongoCollection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.text.Document;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class MongoReaderTester {
     private MongoReader mongoReader;
+
+    //TODO Mock document to give to testInvoiceFiller
+    //TODO Mock document and Invoice to give to testInvoiceLineFiller
 
     @BeforeEach
     public void setupMongoReader() {
@@ -14,7 +21,19 @@ public class MongoReaderTester {
     }
 
     @Test
-    public void testAmountOfDocumentsReturnedFromDatabase() {
-        assertEquals(5, mongoReader.getAllInvoices().size());
+    public void testMongoConnection() {
+        assertEquals(mongoReader.connectToDatabase(), "test");
+    }
+
+    @Test
+    public void testInvoiceFiller(){
+        Document document = new Document();
+
+        assertEquals(mongoReader.fillInvoice(document), "test");
+    }
+
+    @Test
+    public void testInvoiceLineFiller() {
+        assertEquals(mongoReader.fillInvoice(document, invoice), "test");
     }
 }
