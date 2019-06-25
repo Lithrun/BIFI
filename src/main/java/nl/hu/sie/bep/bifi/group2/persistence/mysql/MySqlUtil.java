426cbc34fdc44f46eacb587b2275685df3e61789
Nynke van Koningsveld <NynkevK@users.noreply.github.com>
2019-06-25 12:58:27 +0200

    added logger to MySqlUtils

--- src/main/java/nl/hu/sie/bep/bifi/group2/persistence/mysql/MySqlUtil.java
+++ src/main/java/nl/hu/sie/bep/bifi/group2/persistence/mysql/MySqlUtil.java
@@ -2,6 +2,8 @@ package nl.hu.sie.bep.bifi.group2.persistence.mysql;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MySqlUtil {
     private static SessionFactory sessionFactory;
@@ -12,8 +14,10 @@ public class MySqlUtil {
                 Configuration config = new Configuration();
                 config.configure("mysql/hibernate.cfg.xml");
                 sessionFactory = config.buildSessionFactory();
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (Exception exception) {
+                Logger logger = LoggerFactory.getLogger(MySqlUtil.class);
+                logger.info("getSessionFactory - Exception");
+                exception.printStackTrace();
             }
         }
         return sessionFactory;
